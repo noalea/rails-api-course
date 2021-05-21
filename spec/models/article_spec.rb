e0930cc92016443d1e31ec3c5ab93c3a14c04b62
Noa Lea <noa@noalea.work>
2021-05-21 14:33:33 -0400

    Create a validation test for article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'tests article object' do
-    article = FactoryBot.create(:article)
-    expect(article.title).to eq('Sample Article')
+  describe 'validations' do
+    let(:article) { FactoryBot.build(:article) }
+
+    it 'tests that factory is valid' do
+      expect(article).to be_valid
+    end
+  
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
+
+    it 'has an invalid content' do
+      article.content = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:content]).to include("can't be blank")
+    end
+
+    it 'has an invalid slug' do
+      article.slug = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:slug]).to include("can't be blank")
+    end
+
+    it 'validates the uniqueness of the slug' do
+      article1 = FactoryBot.create(:article)
+      article2 = FactoryBot.build(:article, slug: article1.slug)
+      expect(article2).not_to be_valid
+      expect(article2.errors[:slug]).to include("has already been taken")
+    end
   end
 end
