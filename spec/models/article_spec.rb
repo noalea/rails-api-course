0b9714098fdb6fe1e04097468aedd06fcc6ea060
Noa Lea <noa@noalea.work>
2021-05-21 13:54:03 -0400

    Use FactoryBot to test Article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it "tests a number to be positive" do
-    expect(3).to be_positive()
-    expect(3).to be > 0
+  it 'tests article object' do
+    article = FactoryBot.create(:article)
+    expect(article.title).to eq('Sample Article')
   end
 end
