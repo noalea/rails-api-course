95d5a23550c32e9699c3006523f0ba6e2f646540
Noa Lea <noa@noalea.work>
2021-06-07 17:14:10 -0400

    Handle unauthorized login

--- spec/requests/access_tokens_spec.rb
+++ spec/requests/access_tokens_spec.rb
@@ -1,7 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe "AccessTokens", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe AccessTokensController do
+  describe "#create" do
+    context 'when invalid request' do 
+      let(:error) do
+        {
+          "status": "401",
+          "source": { "pointer": "/code" },
+          "title":  "Authentication code is invalid",
+          "detail": "You must provide valid code in order to exchange it for token."
+        }
+      end
+
+      subject { post '/login' }
+
+      it 'should return status code 401' do
+        subject
+        expect(response).to have_http_status(401)
+      end
+
+      it 'should return proper error body' do
+        subject
+        expect(json[:errors]).to include(error)
+      end
+    end
+
+    context 'when successful request' do
+
+    end
   end
 end
